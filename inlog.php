f627767fcb4e27c1daaa073b7196097a00f8f6c9
Cooper-GaminG <70198785+Cooper-GaminG@users.noreply.github.com>
2021-04-12 08:53:58 +0200

    12-4

---
+++ inlog.php
@@ -0,0 +1,226 @@
+<?php
+session_start();
+//in DB, collectors table, handelingen, daar staan de users die toegang hebben tot de database met de bijbehorende servernaam en username.
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$databasename = "collectorsedition";
+$message = "";
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: login_succes.php");
+    exit;
+}
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+
+  $username = $password = "";
+  $username_err = $password_err = $login_err = "";
+
+    // Processing form data when form is submitted
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    }
+    else{
+        $username = trim($_POST["username"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    }
+    else{
+        $password = trim($_POST["password"]);
+    }
+
+        // Validate Credentials
+        if(empty($username_err) && empty($password_err)){
+            //prepare a select statement
+            $sql = "SELECT UserID, username, password FROM collectors WHERE username = :username";
+    
+
+            if($stmt = $conn->prepare($sql)){
+                // bind variables to the prepared statement as parameters
+                $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+
+                //set parameters
+                $param_username = trim($_POST["username"]);
+
+                //attempt to execute the prepared statement
+                if($stmt->execute()){
+
+                    // check if username exits, if yes then verify password
+                    if($stmt->rowCount() == 1){
+                        if($row = $stmt->fetch()){
+                            $id = $row["UserID"];
+                            $username = $row["username"];
+                            $hashed_password = $row["password"];
+                            if(password_verify($password, $hashed_password)){
+                                // password is correct, so start new session
+                                session_start();
+
+                                //store data in session variables
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["UserID"] = $id;
+                                $_SESSION["username"] = $username;
+
+                                //redirect user to welcome page
+                                header("location: welcome.php");
+                            }
+                            else{
+                                //invalid password, display error message
+                                $login_err = "Invalid username or password.";
+                            }
+                        }
+                    }
+                    else{
+                        //username doesn't exist, display error message
+
+                        $login_err = "Invalid username or password.";
+                    }   
+                }
+                else{
+                    echo "Something went wrong, please try again later.";
+                }
+
+                //close statement
+                unset($stmt);
+            }
+        }
+
+        //close connection
+        unset($conn);
+    }
+
+}
+  catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <title>Login Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/main.css">
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/logintest.css">
+
+        <style>
+            <?php include "main.css" ?>
+            <?php include "login.css" ?>
+            /* using php to connect to a css file */
+        </style>
+
+
+ 
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+        
+      
+    </head>
+    
+    <body >
+
+        <script src="../bootstrap/js/main.js"></script> <!-- links with the javascript document -->
+
+
+        <!--THIS IS THE NAVBAR-->
+    <div class="main">
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand" href="index.php">CollectionHub</a>
+            <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-navigation">
+                <ul class="navbar-nav">
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#"></a>                        
+                        <div class="form-group has-search">
+                            <span class="fa fa-search form-control-feedback"></span>
+                            <input type="text" class="form-control" placeholder="Search">
+                          </div>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+
+        
+
+        <header class="page-header header container-fluid">
+            <!-- <div class="overlay"></div>        -->
+                        
+            <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+
+        <?php 
+        if(!empty($login_err)){
+            echo '<div class="alert alert-danger">' . $login_err . '</div>';
+        }        
+        ?>
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
+        </header>
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+
+        
+    </body>
+    
+</html>
+
+
+
+
+
+
---
+++ login.php
@@ -0,0 +1,175 @@
+<?php
+session_start();
+//in DB, collectors table, handelingen, daar staan de users die toegang hebben tot de database met de bijbehorende servernaam en username.
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$databasename = "collectorsedition";
+$message = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$databasename", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+
+  if(isset($_POST["login"])){
+
+    var_dump($_POST);
+
+        if(empty($_POST["name"]) || empty($_POST["surname"]) || empty($_POST["username"]) || empty($_POST["password"]) || empty($_POST["email"]))
+        {
+            $message = '<label>All fields are required</label>';
+        }
+        else
+        {
+            $query = "INSERT INTO collectors (name, surname, username, password, email) 
+            VALUES (:name, :surname, :username, :password, :email )";
+            $statement = $conn->prepare($query);
+
+            $statement->bindParam(':name', $name);
+            $statement->bindParam(':surname', $surname);
+            $statement->bindParam(':username', $username);
+            $statement->bindParam(':password', $password);
+            $statement->bindParam(':email', $email);
+
+
+            $name      =      $_POST["name"];
+            $surname      =      $_POST["surname"];
+            $username      =      $_POST["username"];
+            $password      =      $_POST["password"];
+            $email      =      $_POST["email"];
+
+
+            $statement->execute();
+
+            $count = $statement->rowCount();
+            if($count > 0)
+            {
+                $_SESSION["name"] = $_POST["username"];
+                header("location:login_succes.php");
+            }
+            else
+            {
+                $message = '<label>Wrong Data</label>';
+            }
+        }
+  }
+}
+  catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <title>Login Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/main.css">
+        <link rel="stylesheet" type="text/css" href="../bootstrap/css/logintest.css">
+
+        <style>
+            <?php include "main.css" ?>
+            <?php include "login.css" ?>
+            /* using php to connect to a css file */
+        </style>
+
+
+ 
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+        
+      
+    </head>
+    
+    <body >
+
+        <script src="../bootstrap/js/main.js"></script> <!-- links with the javascript document -->
+
+
+        <!--THIS IS THE NAVBAR-->
+    <div class="main">
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand" href="index.php">CollectionHub</a>
+            <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="main-navigation">
+                <ul class="navbar-nav">
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#"></a>                        
+                        <div class="form-group has-search">
+                            <span class="fa fa-search form-control-feedback"></span>
+                            <input type="text" class="form-control" placeholder="Search">
+                          </div>
+                    </li> -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+
+
+        
+
+        <header class="page-header header container-fluid">
+            <!-- <div class="overlay"></div>        -->
+                        
+            <div class="container" style="width:500px;">  
+                <?php  
+                if(isset($message))  
+                {  
+                     echo '<label class="text-danger">'.$message.'</label>';  
+                }  
+                ?>  
+                <h3 >Create your CollectionHub account</h3><br />  
+                <form action="login_succes.php"  method="POST">  
+                     <label>Name</label>  
+                     <input type="text" name="name" class="form-control" />  
+                     <br />  
+                     <label>Surname</label>  
+                     <input type="text" name="surname" class="form-control" />  
+                     <br />  
+                     <label>Username</label>  
+                     <input type="text" name="username" class="form-control" />  
+                     <br />  
+                     <label>Password</label>  
+                     <input type="password" name="password" class="form-control" />  
+                     <br />  
+                     <label>Email</label>  
+                     <input type="email" name="email" class="form-control" />  
+                     <br />  
+                     <input type="submit" name="login" class="btn btn-info" value="Sign Up" />  
+                </form>  
+           </div>
+        </header>
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
+
+        
+    </body>
+    
+</html>
