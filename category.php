a84f1a9c7d623758c98c39f13832f2cd0fa69631
riemer <70262649+mrlepel@users.noreply.github.com>
2021-04-12 08:49:21 +0200

    category

---
+++ category.php
@@ -0,0 +1,59 @@
+<?php 
+	include 'API.php';
+ ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row justify-content-center">
+				<form action="API.php" method="POST">
+					<div class="form-group">
+						<label>Add Category</label>
+						<input type="text" name="category_name" class="form-control" required>
+					</div>
+					<div>
+						<button type="submit" class="btn btn-primary" name="save-category">Add</button>
+					</div>
+				</form>
+			</div>
+			<div class="row justify-content-center">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>Your Categories</th>
+							<th></th>
+						</tr>
+					</thead>
+					<?php 
+					while ($row = $result->fetch_assoc()): ?>
+						<tr>
+							<?php if ($update == true): ?>
+								<form name="Edit-Form" method="post" action="API.php">
+									<input type="hidden" name="item-id" value="<?php echo $row['CategoryID'] ?>">
+									<td><input type="text" name="edit_category" value="<?php echo $row['Label']; ?>"></td>
+									<td>
+										<a href="category.php" class="btn btn-danger">Cancel</a>
+										<a type="submit" class="btn btn-info" name="update">Confirm</a>
+									</td>
+								</form>
+							<?php else: ?>
+								<td><?php echo $row['Label'];?></td>
+								<td>
+									<a href="API.php?delete=<?php echo $row['CategoryID']; ?>" class="btn btn-danger" name="delete">Delete</a>
+									<a href="category.php?edit=<?php echo $row['CategoryID']; ?>" name="edit" class="btn btn-info">Edit</a>
+								</td>
+							<?php endif; ?>
+						</tr>
+					<?php endwhile; ?>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
